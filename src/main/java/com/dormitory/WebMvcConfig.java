4575abcfae38a0d50628821fad011e2c0547ffb0
jingom368 <jingom369@gmail.com>
2023-12-08 19:09:19 +0900

    backend

---
+++ src/main/java/com/dormitory/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.dormitory;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// registry.addResourceHandler("/profile/**")
+		// .addResourceLocations("file:///c:/Repository/profile/");
+		
+		// registry.addResourceHandler("/file/**")
+		// .addResourceLocations("file:///c:/Repository/file/");
+	} 
+	
+}
