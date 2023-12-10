145610bad5463afc4696a24becb670c1b4255499
kyungit <ehrud0226@gmail.com>
2023-12-10 19:37:18 +0900

    change

---
+++ src/main/java/com/dormitory/dto/ReservationDTO.java
@@ -0,0 +1,10 @@
+package com.dormitory.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReservationDTO {
+
+}
---
+++ src/main/java/com/dormitory/dto/StatusDTO.java
@@ -0,0 +1,11 @@
+package com.dormitory.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StatusDTO {
+    private int seqno;
+    private String status;
+}
