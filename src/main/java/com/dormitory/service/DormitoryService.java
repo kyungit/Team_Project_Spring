7be2fd588379bcf9997520edb1fe5a5f11a0e96e
kyungit <ehrud0226@gmail.com>
2023-12-12 20:44:23 +0900

    /star

--- src/main/java/com/dormitory/service/DormitoryService.java
+++ src/main/java/com/dormitory/service/DormitoryService.java
@@ -8,11 +8,20 @@ import java.util.List;
 
 
 public interface DormitoryService {
+
+    //==============1. MAIN==================
+
+    //1. 숙소 추천(별점)
+    public List<DormitoryDTO> getDormitoryStar(String keyword);
+
+
+    //
+    public List<RoomDTO> getRoomDetail(String d_code);
+
+
+
+    //====================5. SearchList=================
     // 숙소 정보 조회 (페이지 기주)
     public List<DormitoryDTO> getDormitoryList(int pageNum, String keyword, String type, String star, LocalDate startDate, LocalDate endDate) throws Exception;
 
-    //
-    public List<DormitoryDTO> getDormitoryStar(String keyword);
-
-    public List<RoomDTO> getRoomDetail(String d_code);
 }
