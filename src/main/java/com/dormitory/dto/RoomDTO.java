306b751c9ff3c6f94d1321535a0c388f3aff398a
kyungit <ehrud0226@gmail.com>
2023-12-21 17:34:19 +0900

    23-12-21 17:27 RoomDTO에서 가격 추가

--- src/main/java/com/dormitory/dto/RoomDTO.java
+++ src/main/java/com/dormitory/dto/RoomDTO.java
@@ -9,6 +9,7 @@ public class RoomDTO {
     private String d_code;
     private int r_code;
     private String r_name;
+    private String r_description;
     private int r_num;
     private int r_max_num;
     private String r_img;
