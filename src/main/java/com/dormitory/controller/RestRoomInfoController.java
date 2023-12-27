720a62675671ec7c561772c8f020dc7c47c6769d
jingom368 <jingom369@gmail.com>
2023-12-27 16:36:41 +0900

    12-30 16:36

--- src/main/java/com/dormitory/controller/RestRoomInfoController.java
+++ src/main/java/com/dormitory/controller/RestRoomInfoController.java
@@ -17,6 +17,23 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = { "http://localhost:3000/" })
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dormitory.dto.AmenityDTO;
+import com.dormitory.dto.CancelDTO;
+import com.dormitory.dto.DormitoryDTO;
+import com.dormitory.dto.DormitoryRoomDTO;
+import com.dormitory.dto.ReservationDTO;
+import com.dormitory.dto.ReviewDTO;
+import com.dormitory.dto.RoomTypeDTO;
+import com.dormitory.service.DormitoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = { "http://localhost:3000/" })
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/roomInfo")
@@ -73,6 +90,64 @@ public class RestRoomInfoController {
             }
         }
 
+    //4. 객실 정보
+    // @GetMapping("/roomDetail")
+    // public Map<String, Object> getRoomDetail(DormitoryDTO dormitory, ReservationDTO reservation) {
+    //     System.out.println(reservation.getReservation_checkin());
+    //     System.out.println(reservation.getReservation_checkout());
+    //     String d_code = dormitory.getD_code();
+    //     List<RoomTypeDTO> list = service.getR_Code(d_code);
+    //     Map<String, Object> data = new HashMap<>();
+
+    //     for (int i = 0; i < list.size(); i++) {
+    //         RoomTypeDTO roomCode = list.get(i);
+    //         List<RoomTypeDTO> roomTypeList = service.getUrl(roomCode);
+
+    //         for (int k = 0; k < roomTypeList.size(); k++) {
+    //             RoomTypeDTO room = roomTypeList.get(k);
+    //             //해당 날짜에 예약된 날짜가 있는지 계산하기
+    //             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    //             //db에서 가져온 모든 checkin날짜/checkout날짜
+    //             List<ReservationDTO> DB_reservation = service.getReservationInfoByR_Code(room.getR_code());
+    //             System.out.println("DB_reservation.size() : " + DB_reservation.size());
+    //             //원하는 날짜의 범위
+    //             LocalDate reservation_checkin = reservation.getReservation_checkin();
+    //             LocalDate reservation_checkout = reservation.getReservation_checkout();
+
+    //             if (DB_reservation.size() != 0) {
+    //                 for (int j = 0; j < DB_reservation.size(); j++) {
+    //                     LocalDate checkinDate = DB_reservation.get(j).getReservation_checkin();
+    //                     LocalDate checkoutDate = DB_reservation.get(j).getReservation_checkout();
+    //                     System.out.println("roomTypeList" + k + DB_reservation);
+    //                     System.out.println("checkinDate : " + j + " " + checkinDate);
+    //                     System.out.println("checkoutDate : " + j + " " + checkoutDate);
+
+    //                     for (LocalDate date = reservation_checkin; !date.isAfter(reservation_checkout); date = date
+    //                             .plusDays(1)) {
+    //                         if ((date.isEqual(checkinDate) || date.isAfter(checkinDate))
+    //                                 && date.isBefore(checkoutDate)) {
+    //                             room.setR_status("X");
+    //                             break;
+    //                         } else {
+    //                             room.setR_status("O");
+    //                         }
+    //                     }
+    //                 }
+    //             } else {
+    //                 room.setR_status("O");
+    //             }
+
+    //             // data.put(room.getR_code(), service.getUrl(room)); // => status 안들어감
+    //             data.put(room.getR_code(),room); // => status 들어감
+    //         }
+
+    //     }
+
+    //     return data;
+    // }
+
+
+    // //4. 객실 정보
         // selectedDormitoryList를 사용하여 결과를 생성합니다.
         List<Map<String, Object>> result = selectedDormitoryList.stream().map(dorm -> {
             Map<String, Object> map = new HashMap<>();
@@ -216,6 +291,38 @@ public class RestRoomInfoController {
             if (!reserved.getReservation_checkout().isBefore(checkin)
                     && !reserved.getReservation_checkin().isAfter(checkout)) {
                 count++; // 예약이 중첩됨
+            }
+        }
+        return count; // 예약 가능
+    }
+
+
+
+    private Map<String, String> checkRoomAvailability(List<RoomTypeDTO> roomList, ReservationDTO reservation) {
+        Map<String, String> availability = new HashMap<>();
+        for (RoomTypeDTO room : roomList) {
+            List<ReservationDTO> reservations = service.getReservationInfoByR_Code(room.getR_code()); // r_code에 해당하는 모든 예약 정보
+            int maxRoomCount = service.getRoomCount(room.getR_code());
+            int currentReservationCount = checkIfRoomAvailableCount(reservations, reservation);
+            System.out.println("room.getR_code() : " + room.getR_code());
+            System.out.println("maxRoomCount : " + maxRoomCount);
+            System.out.println("currentReservationCount : " + currentReservationCount);
+            String status = (currentReservationCount < maxRoomCount) ? "O" : "X"; // 기존의 모든 예약 정보와 비교해서 TRUE일 경우 예약 가능, FALSE일 경우 예약 중첩
+            availability.put(room.getR_code(), status);
+        }
+        return availability;
+    }
+
+    private int checkIfRoomAvailableCount(List<ReservationDTO> reservations, ReservationDTO reservation) {
+        LocalDate checkin = reservation.getReservation_checkin();
+        LocalDate checkout = reservation.getReservation_checkout();
+
+        int count = 0;
+        for (ReservationDTO reserved : reservations) { // 기존의 모든 예약 정보와 비교
+            if (!reserved.getReservation_checkout().isBefore(checkin)
+                    && !reserved.getReservation_checkin().isAfter(checkout)) {
+                count++; // 예약이 중첩됨
+
             }
         }
         return count; // 예약 가능
