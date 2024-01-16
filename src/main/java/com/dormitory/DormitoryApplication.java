9a29cecd7d659c662c44a433135105099ea29076
jingom368 <jingom369@gmail.com>
2024-01-16 16:31:52 +0900

    24-01-16 16:31

--- src/main/java/com/dormitory/DormitoryApplication.java
+++ src/main/java/com/dormitory/DormitoryApplication.java
@@ -1,12 +1,17 @@
 package com.dormitory;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DormitoryApplication {
 	public static void main(String[] args) {
-                System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+				
+		Properties props = System.getProperties();
+		props.list(System.out);
 
 		SpringApplication.run(DormitoryApplication.class, args);
 	}
